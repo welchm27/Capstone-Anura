8b4622925cae8495e5932e7e9915fcbad365e9ac
jianxing-ma <jianxing.ma@tlgcohort.com>
2023-08-03 16:18:41 -0400

    added user choice for new game and take user input AB#93#94#96

--- src/main/java/GameLogic.java
+++ src/main/java/GameLogic.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class GameLogic {
     public void execute(){
 
+        String[] stat;
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+        while(userInput.equals("Quit")){
+            System.out.println("Enter [New] for new game or [Save] to load saved game:");
+            userInput = scanner.nextLine();
+            if(userInput.equals("New")){
+                stat = new String[]{};
+            }
+        }
     }
 }
