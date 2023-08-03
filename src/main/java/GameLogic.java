7a0d40ab5a26e30b1447028d2d52f0c7b080c091
Jason Pikturna <Jason.Pikturna@tlgcohort.com>
2023-08-03 13:13:25 -0700

    print splash page at game start, AB#91

--- src/main/java/GameLogic.java
+++ src/main/java/GameLogic.java
@@ -1,5 +1,41 @@
-public class GameLogic {
-    public void execute(){
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class GameLogic {
+
+    public static String filePath = "src/main/resources/SplashPage.txt";
+    public static int[] helpLines = {28, 29, 30, 31};
+
+    public void execute(){
+        //printSplashPage(filePath)
+        //printHelp(filePath, 28, 31)
+
+    }
+    public static void printSplashPage(String filePath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+        }
+    }
+    public static void printHelp (String filePath, int startLine, int endLine ) throws IOException {
+        try (BufferedReader reader =  new BufferedReader(new FileReader(filePath))){
+            String line;
+            int lineNumber = 0;
+
+            while((line = reader.readLine()) != null) {
+                if (lineNumber >= startLine && lineNumber <=endLine) {
+                    System.out.println(line);
+                }
+                lineNumber++;
+                if(lineNumber > endLine){
+                    break;
+                }
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
