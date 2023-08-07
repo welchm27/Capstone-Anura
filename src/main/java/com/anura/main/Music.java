5e6e05dc4a54294f3f6ca7924e18031b931177c3
Lilly <brittany.lilly@TLGcohort.com>
2023-08-07 15:16:48 -0800

    BL AB#128 Music test WIP

---
+++ src/main/java/com/anura/main/Music.java
@@ -0,0 +1,26 @@
+package com.anura.main;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.util.Objects;
+
+class Music {
+
+    public static synchronized void playSound(final String url) {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Clip clip = AudioSystem.getClip();
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+                            //Main.class.getResourceAsStream("/src/main/resources/ShumbaTest.wav"));
+                            Objects.requireNonNull(Main.class.getResourceAsStream(
+                                    "/src/main/resources/Shumba2.wav")));
+                    clip.open(inputStream);
+                    clip.start();
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }).start();
+    }
+}
