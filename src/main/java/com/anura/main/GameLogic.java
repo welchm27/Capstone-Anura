8f95170664e27c26168b017302d08cb853e42718
Lilly <brittany.lilly@TLGcohort.com>
2023-08-08 13:47:22 -0800

    BL AB#130 Music volume can be edited by user

--- src/main/java/com/anura/main/GameLogic.java
+++ src/main/java/com/anura/main/GameLogic.java
@@ -156,10 +156,10 @@ public class GameLogic {
             Music.playSound("/src/main/resources/ShumbaTest.wav");
 //        } else if (musicCommand.equalsIgnoreCase("stop")) {
 //            Music.stopBackgroundMusic();
-//        } else if (musicCommand.equalsIgnoreCase("volume")) {
-//            System.out.println("Enter volume level (low = 0.0 - 1.0 = high):");
-//            float volume = Float.parseFloat(scanner.nextLine());
-//            Music.setVolume(volume);
+        } else if (musicCommand.equalsIgnoreCase("volume")) {
+            System.out.println("Enter volume level (low = 0.0 - 1.0 = high):");
+            float volume = Float.parseFloat(scanner.nextLine());
+            Music.setVolume(volume);
         } else {
             System.out.println("Invalid music command.");
         }
