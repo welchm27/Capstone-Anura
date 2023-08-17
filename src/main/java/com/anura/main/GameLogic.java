7128387ac94241ecf2b9a7a7b224079d87209ce3
welch <welchm27@gmail.com>
2023-08-17 11:20:58 -0400

    Built Jar for v2.1 release, AB#526

--- src/main/java/com/anura/main/GameLogic.java
+++ src/main/java/com/anura/main/GameLogic.java
@@ -29,7 +29,7 @@ public class GameLogic {
         Helper.printFile("SplashPage.txt", Ansi.Color.GREEN);
 
         //Background music starts here
-//        Music.playBGMusic("ShumbaTest.wav");
+        Music.playBGMusic("ShumbaTest.wav");
         Music.setBGMVolume(0.3f);
 
         // ask for new game or saved game
