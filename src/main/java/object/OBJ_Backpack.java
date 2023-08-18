5ef5d1c84893b72b8b0325e69f85619fae57efb9
mendezanderson <N3verloseheart>
2023-08-18 17:02:19 -0400

    NEW items appear on the map, AB#715

---
+++ src/main/java/object/OBJ_Backpack.java
@@ -0,0 +1,16 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class OBJ_Backpack extends SuperObject{
+    public OBJ_Backpack() {
+
+        name = "backpack";
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("../objects/backpack.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/object/OBJ_BottleCap.java
@@ -0,0 +1,16 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class OBJ_BottleCap extends SuperObject {
+    public OBJ_BottleCap() {
+
+        name = "bottleCap";
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("../objects/bottlecap.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
