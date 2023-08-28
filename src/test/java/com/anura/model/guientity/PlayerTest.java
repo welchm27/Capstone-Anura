a19e8b23b98eef7aaa299197e64d06dbbc5c99a8
mendezanderson <N3verloseheart>
2023-08-28 12:29:20 -0400

    created 2 unit tests , AB#590

---
+++ src/test/java/com/anura/model/guientity/PlayerTest.java
@@ -0,0 +1,44 @@
+package com.anura.model.guientity;
+
+import com.anura.controller.KeyHandler;
+import com.anura.view.GamePanel;
+import com.anura.view.TopPanel;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class PlayerTest {
+
+    @Test
+    void testHidePlayer_WithLeafInInventory_ShouldHide() {
+        // Arrange
+        GamePanel gp = new GamePanel(); // You might need to initialize this properly
+        KeyHandler keyHandler = new KeyHandler(gp);
+        TopPanel topPanel = new TopPanel(gp.player); // Initialize properly if needed
+        Player player = new Player(gp, keyHandler, topPanel);
+        player.inventory.add("leaf"); // Simulate having a leaf in inventory
+
+        // Act
+        player.hidePlayer();
+
+        // Assert
+        assertTrue(player.hiding);
+    }
+
+    @Test
+    void testHidePlayer_WithoutLeafInInventory_ShouldNotHide() {
+        // Arrange
+        GamePanel gp = new GamePanel(); // You might need to initialize this properly
+        KeyHandler keyHandler = new KeyHandler(gp);
+        TopPanel topPanel = new TopPanel(gp.player); // Initialize properly if needed
+        Player player = new Player(gp, keyHandler, topPanel);
+        player.inventory.clear(); // Simulate empty inventory
+
+        // Act
+        player.hidePlayer();
+
+        // Assert
+        assertFalse(player.hiding);
+    }
+}
